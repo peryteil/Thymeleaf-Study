70235c5fef7afb29919598c2977927fed6f21a12
peryteil <peryteil@gmail.com>
2025-02-06 14:38:35 +0900

    -자료구조 처리

---
+++ src/main/java/my/app/first_Project/dto/Fruits.java
@@ -0,0 +1,16 @@
+package my.app.first_Project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Fruits {
+    private String fruitsName;
+    private String taste;
+
+}
